44aa059577d64c0ef90151f8a4d7df23754da5cc
Idris Khan <idris.khan303@gmail.com>
2021-02-23 20:08:16 +0000

    Added dependency to Gradle

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,13 +1,54 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import com.example.app.stroage.AppPreferences
 
 class MainActivity : AppCompatActivity() {
+    var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//
+//        // get the actionBar from the layout and hide it
+//        val appBar: ActionBar? = supportActionBar
+//        appBar?.hide()
+//
+        // Hide Actionbar
+        supportActionBar?.hide()
+
+        // Create references for all the Views
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        tvHighScore = findViewById<TextView>(R.id.tv_high_score)
+
+        // Set the onClick listeners
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScoreClick)
+        btnExit.setOnClickListener(this::handleExitEvent)
+
+
+    }
+
+    private fun handleExitEvent(view: View) {
+        finish()
+    }
+
+    private fun onBtnResetScoreClick(view: View) {
+        val preferences = AppPreferences(this)
+        preferences.clearHighScore()
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 }
